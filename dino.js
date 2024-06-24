ca514a9f5c5604d279f8127ccd21fb4404592d43
Subha9932 <bossubhadip19@gmail.com>
2024-06-24 22:43:31 +0530

    first commit

---
+++ dino.js
@@ -0,0 +1,34 @@
+let score = 0;
+let cro = true;
+auel=new Audio('Maan Meri Jaan_320(PagalWorld.com.pe).mp3');
+setInterval(() => {
+    let dino = document.querySelector('.dino');
+    let gameover = document.querySelector('.gameover');
+    let mons = document.querySelector('.mons');
+
+    let dx = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'));
+    let dy = parseInt(window.getComputedStyle(dino, null).getPropertyValue('top'));
+
+    let mx = parseInt(window.getComputedStyle(mons, null).getPropertyValue('left'));
+    let my = parseInt(window.getComputedStyle(mons, null).getPropertyValue('top'));
+
+    let offx = Math.abs(dx - mx);
+    let offy = Math.abs(dy - my);
+
+    if (offx < 93 && offy < 52) {
+        gameover.style.visibility = 'visible';
+        mons.classList.remove('monsani');
+    } else if (offx<145 && cro) {
+        score = score + 1;
+        upscor(score);
+        cro = false;
+        setTimeout(() => {
+            cro = true;
+        }, 100);
+    }
+}, 10);
+
+function upscor(score) {
+    let scoreCon = document.querySelector('.scoreCon');
+    scoreCon.innerHTML = "Your score: " + score;
+}
